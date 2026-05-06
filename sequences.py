7bd8ecfbe3d8066b4cc6f01f423dfa3de4478d79
Nina Macurová <283216@vutbr.cz>
2026-05-06 09:29:42 +0200

    pridavam riesenie DNA aa RNA

--- sequences.py
+++ sequences.py
@@ -12,12 +12,6 @@ class Sequence:
     def __str__(self):
         return f"[{self.name}] délka={self.length()} nt, začátek: {self.sequence[:8]}..."
 
-seq = Sequence("testovací", "acgtagctagc")
-print(seq)  # [testovací] délka=11 nt, začátek: ACGTAGCT...
-print(seq.length())  # 11
-print(seq.sequence)  # ACGTAGCTAGC – automaticky převedeno na velká písmena
-
-
 class DNASequence(Sequence):
     def gc_content(self):
         gc = self.sequence.count("G") + self.sequence.count("C")
@@ -50,22 +44,27 @@ class DNASequence(Sequence):
     # Transkripce DNA → RNA
     def to_rna(self):
         return RNASequence(self.name, self.sequence.replace("T", "U"))
+
 class RNASequence(Sequence):
 
     # Validace RNA
     def is_valid(self):
         return set(self.sequence) <= {"A", "C", "G", "U"}
 
-    # Rozdělení na kodony
+
+# Rozdělení na kodony
     def codons(self):
         return [self.sequence[i:i+3] for i in range(0, len(self.sequence) - 2, 3)]
+
 
     # Hledání start kodonu AUG
     def find_start_codon(self):
         return self.sequence.find("AUG")
 
-dna1 = DNASequence("mini", "ACCGGGTT")
-print(dna1)
-print("GC obsah:", dna1.gc_content())
-print("Počty bází:", dna1.base_counts())
-print("Validní?", dna1.is_valid())
+dna = DNASequence("gen_01", "CCATGGCTTAA")
+
+rna = dna.to_rna()
+print(rna)
+print(rna.is_valid())
+print(rna.find_start_codon())
+print(rna.codons())
